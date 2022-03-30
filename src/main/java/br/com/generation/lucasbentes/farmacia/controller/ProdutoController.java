8b80dcab2142fdd297622dc96f3652e46dcdf776
lucas bentes <lucasbentes16@gmail.com>
2022-03-30 17:11:26 -0300

    add de pesquisa por valor

--- src/main/java/br/com/generation/lucasbentes/farmacia/controller/ProdutoController.java
+++ src/main/java/br/com/generation/lucasbentes/farmacia/controller/ProdutoController.java
@@ -42,6 +42,11 @@ public class ProdutoController {
 		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
 	}
 	
+	@GetMapping("/valor/{valor}")
+	public ResponseEntity<List<Produto>> getByValor(@PathVariable String valor){
+		return ResponseEntity.ok(repository.findAllByValor(valor));
+	}
+	
 	@PostMapping
 	public ResponseEntity<Produto> post(@RequestBody Produto produto){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(produto));
--- src/main/java/br/com/generation/lucasbentes/farmacia/repository/ProdutoRepository.java
+++ src/main/java/br/com/generation/lucasbentes/farmacia/repository/ProdutoRepository.java
@@ -9,5 +9,6 @@ import br.com.generation.lucasbentes.farmacia.model.Produto;
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+	public List<Produto> findAllByValor(String valor);  // Como a entrada e numeros nao vai precisar de ContainingIgnoreCase
 	
 }
