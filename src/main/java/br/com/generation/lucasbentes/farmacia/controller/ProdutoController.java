f9c8e3d2c2b5108aac3ebb3ae215213633aaff46
lucas bentes <lucasbentes16@gmail.com>
2022-03-30 14:43:15 -0300

    add correcoes

--- src/main/java/br/com/generation/lucasbentes/farmacia/controller/ProdutoController.java
+++ src/main/java/br/com/generation/lucasbentes/farmacia/controller/ProdutoController.java
@@ -38,8 +38,13 @@ public class ProdutoController {
 	}
 	
 	@GetMapping("/nome/{nome}")
-	public ResponseEntity<List<Produto>> getByID(@PathVariable String nome){
+	public ResponseEntity<List<Produto>> getByNome(@PathVariable String nome){
 		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	@GetMapping("/valor/{valor}")
+	public ResponseEntity<List<Produto>> getByValor(@PathVariable String valor){
+		return ResponseEntity.ok(repository.findAllByValorContainingIgnoreCase(valor));
 	}
 	
 	@PostMapping
--- src/main/java/br/com/generation/lucasbentes/farmacia/repository/ProdutoRepository.java
+++ src/main/java/br/com/generation/lucasbentes/farmacia/repository/ProdutoRepository.java
@@ -9,5 +9,6 @@ import br.com.generation.lucasbentes.farmacia.model.Produto;
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+	public List<Produto> findAllByValorContainingIgnoreCase(String valor);
 	
 }
